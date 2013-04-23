3bc6864bbc0a7808f3f50599a74e0b78c4031346
mkanicki <maciej.kanicki90@gmail.com>
2013-04-23 23:00:37 +0300

    Update registration.php
    troll

--- projekt/ppz-gazeta/registration.php
+++ projekt/ppz-gazeta/registration.php
@@ -13,8 +13,19 @@
             header("Location: index.php?msg=1");
         } else {
             // bledy
+            /**
+             * w przypadku gdy $wynik nie zwroci true, czyli w classes/Uzytkownicy.php funckja zapisz() zwroci jakies bledy
+             * do smarty przypisujemy $bledy wykryte przy zapisywaniu (zapisz())
+             * dzieki temu i zastosowaniu w templates/layout_register.tpl w input pozycji np. class="{$bledy.Imie}"
+             * mozemy podswietlic na czerwono niewypelniony input
+             */
             $smarty->assign('bledy', $wynik);
         }
+        /**
+         * Dzieki temu mozemy w templates/layout_register.tpl uzywac np. value="{$uzytkownik.Imie}" w input
+         * co pozwala na zachowywanie czesci informacji w trakcie rejestracji, w przypadku gdy uzytkownik
+         * wypelni jedynie czesc potrzebnych danych do zaakceptowania rejestracji
+         */
         $smarty->assign('uzytkownik', $_POST);
         
     }    
