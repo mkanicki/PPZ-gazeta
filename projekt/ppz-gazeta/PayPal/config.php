8d870236fbbd04ec4547c2209c0d425fd57805b2
astrusiew <alpast91@gmail.com>
2013-05-14 22:15:38 +0200

    usuwanie starej wersji

--- projekt/ppz-gazeta/PayPal/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$PayPalMode 			= 'sandbox'; // sandbox or live
-$PayPalApiUsername 		= 'kosciesza16-facilitator_api1.wp.pl'; //PayPal API Username
-$PayPalApiPassword 		= '1364242495'; //Paypal API password
-$PayPalApiSignature     	= 'AcE3Ju0C.lvLba5.4aIOtIvUn.JAABqz1ZwhuhSYANOe-k4GGbFL3.hC'; //Paypal API Signature
-$PayPalCurrencyCode     	= 'USD'; //Paypal Currency Code
-$PayPalReturnURL 		= 'http://localhost:81/ppz-gazeta/basket.php';//'localhost/process.php'; //Point to process.php page
-$PayPalCancelURL 		= 'http://localhost:81/ppz-gazeta/basket.php';//'localhost/cancel_url.php'; //Cancel URL if user clicks cancel
-?>
