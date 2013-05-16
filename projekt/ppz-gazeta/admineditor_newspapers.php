34177eefa027fc6810622c85920a38c7b32b2f2c
mkanicki <maciej.kanicki90@gmail.com>
2013-05-16 15:07:10 +0300

    Update admineditor_newspapers.php
    nie to skopiowałem

--- projekt/ppz-gazeta/admineditor_newspapers.php
+++ projekt/ppz-gazeta/admineditor_newspapers.php
@@ -1,17 +1,19 @@
 <?php
     require_once 'classes/Init.php';
     require_once 'classes/Uzytkownicy.php';
+    require_once 'classes/Wydanie.php';
+
+    
     
     $init = new Init();
-    $init->sprawdzAdmin(); // sprawdza, czy admin
-    
+    $init->sprawdzAdmin(); // sprawdza, czy nie czytelnik
     $smarty = $init->getSmarty();
-    $uzytkownicy = new Uzytkownicy();
     
-    $smarty->assign('uzytkownicy', $uzytkownicy->pobierzListe());
-    $smarty->assign('obiekt' , $smarty->fetch("layout_admin_index.tpl"));
+    $view = new Wydanie();
+    
+    //var_dump($view->pokazWszystko());
+    $smarty->assign('wydania', $view->pokazWszystko());
+    $smarty->assign('obiekt' , $smarty->fetch("layout_admineditor_newspapers.tpl"));
 
     $smarty->display('layout_admin.tpl');
 ?>
-
-
